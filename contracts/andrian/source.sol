033541b9257aac159f88e2fe6ccd4eab2e668638
drilow <drilow@hotmail.com>
2016-11-12 21:26:49 +0100

    adrian/source.sol is the final version to be

--- contracts/andrian/source.sol
+++ contracts/andrian/source.sol
@@ -1,55 +1,62 @@
 pragma solidity ^0.4.2;
 
 contract lottery {
-
+  
     address owner;
-
 
     string greeting;
     mapping (address => string) realName;
     mapping (string => address) addFromName;
     mapping (address => uint256) participants;
     mapping (uint256 => address) listofParticipants;
-    uint256 id = 0;
+    uint256 id = 1;
+    uint hashed = 0;
     address winner;
     bool ended = false;
-
+    
     event showAdr (string msg, address a);
-    //event showString (string a);
     modifier beforeEnded () {if(!ended) _; }
     modifier onlyOwner () {if(msg.sender==owner) _; }
     modifier onlyOwnerAfterEnd () {if(msg.sender==owner && ended) _; }
-
-
-    function endGame () onlyOwner  {
-      ended = true;
-      //chooseWinner();
-      //kill();
+    
+    function initialize() {
+      owner = msg.sender;
     }
-
+    
     function addPlayer (string fullname) beforeEnded  {
-      uint256 exists = participants[msg.sender];
-      if ((id==0 || exists==0 && id>0 && listofParticipants[0]!=msg.sender) && addFromName[fullname]==0) {
+
+      if (participants[msg.sender]==0 && addFromName[fullname]==0 && msg.sender!=owner) {
         participants[msg.sender] = id;
         listofParticipants[id] = msg.sender;
         realName[msg.sender] = fullname;
         addFromName[fullname]=msg.sender;
         id++;
+        updateHash(msg.sender);
         showAdr("Participant Added: ",msg.sender);
       } else throw;
     }
+    
+    function updateHash(address a) internal {
 
-    function randomFrom (uint256 range) internal returns (uint256){
-
-      uint256 result = 0;
-      result = uint256(sha3(block.blockhash(block.number-1)))%range;
-      return result;
-
+      if(now % 2 == 0) {
+        hashed = hashed * uint(sha3(a));
+        showAdr("Hashed with Adress: ",msg.sender);
+      } else {
+        hashed = hashed * uint(sha3(now));
+        showAdr("Hashed with Time: ",address(now));
+      }
     }
-
+    
+    function endGame () onlyOwner  {
+      ended = true;
+    }
+      
     function chooseWinner() onlyOwnerAfterEnd {
-      winner = listofParticipants[randomFrom(id)];
+      winner = listofParticipants[hashed%id+1];
       showAdr(realName[winner],winner);
+    }
+    function getWinner () {
+      return winner;
     }
     function kill () onlyOwner {selfdestruct(owner);}
 }
