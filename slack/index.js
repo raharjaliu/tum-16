055027aee34c4c08d5e7102d84c8a2d8822b623c
Askar Akhmerov <askar.akhmerov@camunda.com>
2016-11-12 16:05:09 +0100

    add basic parsing

--- slack/index.js
+++ slack/index.js
@@ -9,10 +9,7 @@ web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
 var coinbase = web3.eth.coinbase;
 
-var balance = web3.eth.getBalance(coinbase);
-console.log(balance.toString(10));
-
-var bot_token = process.env.SLACK_BOT_TOKEN || '';
+var bot_token = process.env.SLACK_BOT_TOKEN || 'xoxb-104489032486-ra7lArT3c8MQHvLB1JuF0HWq';
 
 var slack = new RtmClient(bot_token, {
   logLevel: 'error',
@@ -21,18 +18,47 @@ var slack = new RtmClient(bot_token, {
   autoMark: true
 });
 
+var me = null;
+var currentLottery = null;
 
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
 slack.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+  for (var user_id in slack.dataStore.users) {
+    var user = slack.dataStore.users[user_id];
+    if (user.name === 'slottery') {
+      me = user;
+      console.log(me);
+      break;
+    }
+  }
 });
 
 slack.start();
 
 var channel = "#general";
 
-slack.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  console.log('Message:', message); //this is no doubt the lamest possible message handler, but you get the idea
+var processAction = function (message) {
   var channel = slack.dataStore.getChannelGroupOrDMById(message.channel);
-  slack.sendMessage(`Hello ${channel.name}!`, channel.id);
+  if (message.text.indexOf('lottery') >= 0 && message.text.indexOf('running') >= 0) {
+      slack.sendMessage(`Hello <@${message.user}>!`, channel.id);
+      if (currentLottery === null) {
+        slack.sendMessage(`there is no lottery running`, channel.id);
+      } else {
+        slack.sendMessage(`there is a lottery running`, channel.id);
+      }
+  } else if (message.text.indexOf('balance') >= 0) {
+      slack.sendMessage(`Hello <@${message.user}>!`, channel.id);
+      var balance = web3.eth.getBalance(coinbase);
+      slack.sendMessage(`your balance is ${balance.toString(10)}`, channel.id);
+  } 
+}
+
+slack.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+  if (me !== null) {
+    if (message.text.indexOf(me.id) >= 0) {
+      processAction(message);
+    }
+  }
+  console.log('Message:', message);
 });
