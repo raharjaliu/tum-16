df250062f428b7c68b544e6a37ebf6f069bfd096
apodznoev <apodznoev@yandex.ru>
2016-11-13 15:26:47 +0100

    -fix _xxx_

--- contracts/final/source.sol
+++ contracts/final/source.sol
@@ -2,24 +2,15 @@ pragma solidity ^0.4.2;
 
 contract lottery {
 
-    struct Player {
-        string _telephonNumber;
-    }
-
-    Player NONE = Player({
-        _telephonNumber: ''
-
-    });
-
     address owner;
 
-    Player[] public players;
+    string[] public players;
     uint public hashed = 1;
     string public winnerNummer;
     bool public ended = false;
 
 
-    event Debug1 (string msg, address data);
+    event Debug1 (string msg, string data);
     event Debug2 (string msg, uint data);
     event SendWinner (string telephonNumber);
 
@@ -31,31 +22,19 @@ contract lottery {
     }
 
     function addPlayer (string telephonNumber) public beforeEnded  {
-        Player memory existing = getExisting(msg.sender, telephonNumber);
-        if (isNull(existing)) {
-            players.push(Player({
-                _telephonNumber:telephonNumber
-            }));
+        if (!exists(telephonNumber)) {
+            players.push(telephonNumber);
             updateHash(msg.sender);
             Debug2("Hash changed new is:: ", hashed);
-            Debug1("Participant Added: ", msg.sender);
+            Debug1("Participant Added: ", telephonNumber);
         } else throw;
-    }
-
-    function isNull(Player p) internal returns(bool) {
-        bytes memory b = bytes(p._telephonNumber);
-        if(b.length > 1)
-            return false;
-
-        return true;
     }
 
 
     function chooseWinner() public onlyOwner {
       ended = true;
-      Player winner = players[hashed % players.length];
-      winnerNummer = winner._telephonNumber;
-      SendWinner(winner._telephonNumber);
+      winnerNummer = players[hashed % players.length];
+      SendWinner(winnerNummer);
     }
 
     function getWinner () public returns(string add) {
@@ -65,19 +44,19 @@ contract lottery {
     function kill () public onlyOwner {selfdestruct(owner);}
 
     function reset() public onlyOwner {
-      ended = false;
+      players.length = 0;
       delete players;
       owner = 0;
       winnerNummer = '';
     }
 
-     function getExisting(address _address, string _telephonNumber) internal returns (Player) {
+     function exists(string _telephonNumber) internal returns (bool) {
         for(uint i = 0; i < players.length; i++) {
-            Player player = players[i];
-            if(stringsEqual(player._telephonNumber, _telephonNumber))
-                return player;
+            string player = players[i];
+            if(stringsEqual(player, _telephonNumber))
+                return true;
         }
-        return NONE;
+        return false;
     }
 
     function stringsEqual(string storage _a, string memory _b) internal returns (bool) {
@@ -95,10 +74,8 @@ contract lottery {
     function updateHash(address a) internal {
       if(now % 2 == 0) {
         hashed = hashed * uint(sha3(a));
-        Debug1("Hashed with Address: ", msg.sender);
       } else {
         hashed = hashed * uint(sha3(now));
-        Debug1("Hashed with Time: ", address(now));
       }
     }
 }
